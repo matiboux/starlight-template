f9f40be3e84863a01af99eda6ae1c23d68ecddd2
Matiboux <matiboux@gmail.com>
2025-08-13 23:35:42 +0200

    Add sidebar config

---
+++ app/app/src/content/docs/sidebar.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('@astrojs/starlight/types').StarlightUserConfig} */
+const sidebar = [
+	{
+		label: 'Welcome',
+		translations: {
+			fr: 'Bienvenue',
+		},
+		slug: 'index',
+	},
+]
+
+export default sidebar
